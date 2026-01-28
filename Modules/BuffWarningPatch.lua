2e2e2e74f5374114eef19eddcc0d95c68cfc6ffe
Gerrit Alex <admin@gerritalex.de>
2026-01-28 19:54:36 +0100

    also disable for ExternalDefensives

--- Modules/BuffWarningPatch.lua
+++ Modules/BuffWarningPatch.lua
@@ -3,3 +3,4 @@ local addonName, Private = ...
 -- this turns off the alpha change of auras
 BuffFrame.AuraContainer.GetAuraWarningAlphaForDuration = nil
 DebuffFrame.AuraContainer.GetAuraWarningAlphaForDuration = nil
+ExternalDefensivesFrame.AuraContainer.GetAuraWarningAlphaForDuration = nil
