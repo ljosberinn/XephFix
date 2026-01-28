d123cc69ae38b39d40b38e6e0ff9679c6e78af47
Gerrit Alex <admin@gerritalex.de>
2026-01-28 20:04:06 +0100

    update readme

--- Modules/GearUpgradeReminder.lua
+++ Modules/GearUpgradeReminder.lua
@@ -6,57 +6,14 @@ local addonName, Private = ...
 --     return acc
 -- }, {})))
 
-local isMidnight = select(4, GetBuildInfo()) >= 120000
-
-local bonusToTierMap = isMidnight and {
+local bonusToTierMap = {
 	[13332] = "Raid Finder",
 	[13334] = "Heroic",
 	[13335] = "Mythic",
-} or {
-	[12265] = "Explorer",
-	[12266] = "Explorer",
-	[12267] = "Explorer",
-	[12268] = "Explorer",
-	[12269] = "Explorer",
-	[12270] = "Explorer",
-	[12271] = "Explorer",
-	[12272] = "Explorer",
-	[12274] = "Adventurer",
-	[12275] = "Adventurer",
-	[12276] = "Adventurer",
-	[12277] = "Adventurer",
-	[12278] = "Adventurer",
-	[12279] = "Adventurer",
-	[12280] = "Adventurer",
-	[12281] = "Adventurer",
-	[12282] = "Veteran",
-	[12283] = "Veteran",
-	[12284] = "Veteran",
-	[12285] = "Veteran",
-	[12286] = "Veteran",
-	[12287] = "Veteran",
-	[12288] = "Veteran",
-	[12289] = "Veteran",
-	[12290] = "Champion",
-	[12291] = "Champion",
-	[12292] = "Champion",
-	[12293] = "Champion",
-	[12294] = "Champion",
-	[12295] = "Champion",
-	[12296] = "Champion",
-	[12297] = "Champion",
-	[12350] = "Hero",
-	[12351] = "Hero",
-	[12352] = "Hero",
-	[12353] = "Hero",
-	[12354] = "Hero",
-	[12355] = "Hero",
-	[12356] = "Myth",
-	[12357] = "Myth",
-	[12358] = "Myth",
-	[12359] = "Myth",
-	[12360] = "Myth",
-	[12361] = "Myth",
+	[13443] = "Hero",
+	[13444] = "Hero",
+	[13445] = "Myth",
+	[13446] = "Myth",
 }
 
 -- copy(JSON.stringify(Object.values(JSON.parse($0.textContent)).filter(x => x.upgrade?.seasonId === 30).reduce((acc, data) => {
@@ -77,38 +34,21 @@ local bonusToTierMap = isMidnight and {
 --     return acc
 -- }, {})).replaceAll(':', '=').replaceAll('"', ''))
 
-local tiers = isMidnight
-		and {
-			Adventurer = { min = 220, max = 237 },
-			Veteran = { min = 233, max = 250 },
-			Champion = { min = 246, max = 263 },
-			Hero = { min = 259, max = 276 },
-			Myth = { min = 272, max = 289 },
-		}
-	or {
-		Explorer = { min = 642, max = 665 },
-		Adventurer = { min = 655, max = 678 },
-		Veteran = { min = 668, max = 691 },
-		Champion = { min = 681, max = 704 },
-		Hero = { min = 694, max = 710 },
-		Myth = { min = 707, max = 730 },
-	}
+local tiers = {
+	Adventurer = { min = 220, max = 237 },
+	Veteran = { min = 233, max = 250 },
+	Champion = { min = 246, max = 263 },
+	Hero = { min = 259, max = 276 },
+	Myth = { min = 272, max = 289 },
+}
 
-local craftedBonusIds = isMidnight
-		and {
-			[12066] = true, -- Radiance Crafted
-			-- https://wago.tools/db2/ItemBonus?filter%5BValue_2%5D=2061%7C2062%7C2063&page=1
-			-- presumably 15587?
-		}
-	or {
-		[9498] = true, -- Dream Crafted
-		[10249] = true, -- Awakened Crafted
-		[10222] = true, -- Omen Crafted
-		[12040] = true, -- Fortune Crafted
-		[12050] = true, -- Starlight Crafted
-	}
+local craftedBonusIds = {
+	[12066] = true, -- Radiance Crafted
+	-- https://wago.tools/db2/ItemBonus?filter%5BValue_2%5D=2061%7C2062%7C2063&page=1
+	-- presumably 15587?
+}
 
-local crestFreeItemLevelUpgradeThreshold = isMidnight and 999 or 580
+local crestFreeItemLevelUpgradeThreshold = 999
 
 local function GetUpgradeTrack(bonusIds)
 	for i = 1, #bonusIds do
@@ -177,7 +117,7 @@ EventRegistry:RegisterFrameEventAndCallback("PLAYER_EQUIPMENT_CHANGED", function
 		if itemLoc:IsValid() then
 			local currentItemLevel = C_Item.GetCurrentItemLevel(itemLoc)
 
-			if currentItemLevel >= (isMidnight and tiers.Adventurer.min or tiers.Explorer.min) then
+			if currentItemLevel >= tiers.Adventurer.min then
 				local itemLink = C_Item.GetItemLink(itemLoc)
 
 				if itemLink then
--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-Collection of former utiltiy WeakAuras or similar:
+Collection of former utiltiy WeakAuras or similar.
+
+## How To Disable A Module For Yourself
+
+- edit the `XephFix.toc` file and remove the file name you want to disable, then reload, that's all
+
+## Modules
+
+The following modules are enabled for everyone by default:
 
 - AddonSuiteProfileSwapReminder
+  - only loads if you have the addon `AddonSuite` installed
   - reminder for myself to swap addon profile depending on whether I'm in raid or not
 - AutoGreet
   - the equivalent of /p hi
@@ -8,30 +17,34 @@ Collection of former utiltiy WeakAuras or similar:
   - removes the stupid alpha change for expiring auras
 - CharacterSheetImprovements
   - extends the default ui character sheed with enchants, gems, and item level
-- CharacterStatsFormatting
-  - replaced by EQoL implementing this
-- CleanNamesInInstances
-  - port of https://wago.io/M13VlqtM-
-- CurrentExpansionOnlyAuctionHouseFilter
-  - port of https://wago.io/FW6qfBuIH
-  - EnhanceQoL -> Economy > Auction House includes this
 - DraggableFrames
   - makes a couple additional default ui frames draggable
-- EncounterProgressWhisper
+    - applies to Spellbook and the Group Finder
 - GearUpgradeRankTooltipRenamer
   - port of https://wago.io/78z7JtUxS
 - GearUpgradeReminder
   - port of https://wago.io/DA3OzoIsi
+- HiddenQuestCleanup
+  - automatically unwatches hidden watched quests
+  - sounds stupid - but this, based on unknown factors - can be a significant performance improvement, I personally gained ~9 fps in dornogal
 - MDTSim
   - port of https://wago.io/LTwpBiuz_
+- TalentReminders
+  - plays TTS when entering Midnight dungeons and you're missing certain talents
+  - only implemented for evoker, doesn't do anything if you're not an evoker
+- TurnSpeed
+  - reverts turn speed back to 180 should it get lowered
+- WeakAuras
+  - /wa binding for the cooldown manager...
+
+The following modules are ONLY ACTIVE IF YOUR CHARACTER NAME MATCHES MINE:
+
+- CleanNamesInInstances
+  - port of https://wago.io/M13VlqtM-
 - Stopwatch
   - modifies the ingame stopwatch to act as combat timer. has ui elemenets hidden such as hours, buttons and background
 - TargetShield
   - shows remaining amount of absorb on the target
-- TalentReminders
-- TurnSpeed
-  - reverts turn speed back to 180 should it get lowered
 - UIScale
   - port of !UIScale addon, but doesn't throw an error when reloading ui mid-combat
-- WeakAuras
-  - /wa binding for the cooldown manager...
+  - set to 1440p resolution
