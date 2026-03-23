60b85209118ffdf0e64a81a3cc1d360b016b8a9d
Gerrit Alex <admin@gerritalex.de>
2026-03-24 00:03:26 +0100

    add fast loot

---
+++ Modules/FastLoot.lua
@@ -0,0 +1,31 @@
+local _, Private = ...
+
+table.insert(Private.LoginFnQueue, function()
+	if C_AddOns.DoesAddOnExist("Plumber") or C_AddOns.DoesAddOnExist("Leatrix_Plus") then
+		return
+	end
+
+	local frame = CreateFrame("Frame")
+	frame.lastLoot = 0
+
+	frame:RegisterEvent("LOOT_READY")
+	frame:RegisterEvent("LOOT_BIND_CONFIRM")
+	frame:SetScript("OnEvent", function(self, event, ...)
+		if event == "LOOT_READY" then
+			if GetTime() - self.lastLoot < 0.3 then
+				return
+			end
+
+			self.lastLoot = GetTime()
+
+			if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+				for i = GetNumLootItems(), 1, -1 do
+					LootSlot(i)
+				end
+			end
+		elseif event == "LOOT_BIND_CONFIRM" then
+			local slot = ...
+			ConfirmLootSlot(slot)
+		end
+	end)
+end)
