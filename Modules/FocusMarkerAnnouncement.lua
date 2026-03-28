41db0aa8f8867082f0503a21cf9d490b7127b837
Gerrit Alex <admin@gerritalex.de>
2026-03-28 23:03:02 +0100

    patch focus marker announcement

--- Modules/FocusMarkerAnnouncement.lua
+++ Modules/FocusMarkerAnnouncement.lua
@@ -1,31 +1,50 @@
 local addonName, Private = ...
 
 table.insert(Private.LoginFnQueue, function()
-	if not Private.IsXeph then
-		return
-	end
+	local macroName = "focus"
 
 	local function ExtractMarkerFromMacro()
 		for i = 1, GetNumMacros() do
 			local name, icon, body = GetMacroInfo(i)
 
-			if name == "focus" then
+			if name == macroName then
 				for line in body:gmatch("[^\n]+") do
 					if string.find(line, "/tm") ~= nil then
-						return tonumber(line:match("^/tm%s+(%d+)$"))
+						return tonumber(line:match("^/tm%s+.-(%d+)$"))
 					end
 				end
+
+				return nil
 			end
 		end
 
-		error("Could not find /tm in macro")
+		return nil
 	end
+
+	print(
+		"[XephFix] FocusMarkerAnnouncement attempts to extract the target marker you're using from a macro called 'focus'. If you don't use focus, it won't do anything. If you wish to disable this message, disable FocusMarkerAnnouncement.lua"
+	)
 
 	local frame = CreateFrame("Frame")
 	frame:RegisterEvent("READY_CHECK")
 	frame:SetScript("OnEvent", function(self, event, ...)
+		if IsInRaid() then
+			return
+		end
+
+		-- cannot send chat mid key
+		if C_ChallengeMode.IsChallengeModeActive() then
+			return
+		end
+
 		if event == "READY_CHECK" then
-			C_ChatInfo.SendChatMessage(string.format("My Focus Marker is {rt%d}", ExtractMarkerFromMacro()), "PARTY")
+			local markerId = ExtractMarkerFromMacro()
+
+			if markerId == nil then
+				return
+			end
+
+			C_ChatInfo.SendChatMessage(string.format("My Focus Marker is {rt%d}", markerId), "PARTY")
 		end
 	end)
 end)
